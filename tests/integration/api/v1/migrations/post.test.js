5a183aad985e39620277d6c2c1be5769c66b4f4b
Leandro Ribeiro <souza.ribeiro92@gmail.com>
2024-07-02 00:03:25 +0000

    update `post.test.js`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,7 +1,7 @@
 test("POST to /api/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
-  });
+  }); 
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
